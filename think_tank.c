8e6741e1d348d098f1c44d0f9d3bf7dfe2f7cc52
Soryung Lee <thfudwofud@naver.com>
2015-10-14 13:36:55 +0900

    add comments, etc
    영어로 주석 추가도 하고 필요없는 부분은 삭제하였고 컴파일러의 경고도 하나도 없도록 수정하였습니다.

--- think_tank.c
+++ think_tank.c
@@ -1,19 +1,34 @@
+/*
+	Name: Soryung Lee(csddochi)
+	Objectives: The program for generating three random numbers and
+			printing the three words asociated with the corresponding numbers.
+	Program Name: think tank (The Think Tank of Edward de Bono)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define MAX 25457
+#define MAX 25457 // last index of the word in noun.txt
 
-int main(int argc, char** argv) {
-	char word[1024] = { 0, };
+int main(int argc, char** argv) 
+{
+	char word[128] = { 0, };
 	int num[3] = { 0, };
-	int i, j, temp;
-	FILE* f = 0;
+	int i, temp;
+	FILE* fp = fopen("noun.txt", "r");
+	// Download the original version of noun.txt 
+	//	in National Institute of the Korean Language.
+	// This file is processed according to 
+	//	the 'substantives_detail.txt' file by me.
+	int this_index = 0;
+	int get_index = 0;
 
-	srand(time(NULL));
+	// A part for generating three random numbers except overlap
+	srand((unsigned int)time(NULL)); // for avoiding c4244 warning
 	num[0] = rand() % MAX;
-	for (;;) {
+	for (;;) { 
 		num[1] = rand() % MAX;
 		num[2] = rand() % MAX;
 		if (num[1] == num[0])
@@ -26,45 +41,30 @@ int main(int argc, char** argv) {
 			break;
 	}
 
-	//srand(time(NULL));
-	//for (i = 0; i < 3; i++)
-	//{
-	//	num[i] = rand() % 10;
-	//}
-	//for (i = 0; i < 3; i++)
-	//{
-	//	for (j = 1; j < 3; j++)
-	//	{
-	//		if (num[i] == num[j])
-	//		{
-	//			num[i] = rand() % 10;
-	//		}
-	//	}
-	//}
-
-
+	// A portion for arranging to the numbers in ascending order
 	for (i = 0; i < 2; i++) {
-		if (num[i] > num[i + 1]) {
+		if (num[i] >= num[i + 1]) {
 			temp = num[i + 1];
 			num[i + 1] = num[i];
 			num[i] = temp;
 		}
 	}
-
+	// A part for outputting the numbers in ascending
 	printf("%d %d %d\n", num[0], num[1], num[2]);
 
-	char ch;
-	int this_index = 0;
-	int get_index = 0;
-
-	f = fopen("noun.txt", "r");
-
-	while (f != EOF)
+	printf("<ThinkTank> There are today words:\n");
+	while (!feof(fp)) // fp != EOF -> warning C4047
 	{
-		fscanf(f, "%d %s", &get_index, word);
+		fscanf(fp, "%d %s", &get_index, word);
+		
 		if (get_index == num[this_index])
+		{
 			printf("%d %s\n", get_index, word), this_index++;
-		if (this_index == 3)break;
+		}
+		
+		if (this_index == 3)
+		{
+			break; // while loop exit if print three words
+		}
 	}
-	//system("pause");
 }
